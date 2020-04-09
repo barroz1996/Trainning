f72d02c6ec5241066f07731333bed0068d3ca8bb
Yaron Zelinger <yaronze@post.bgu.ac.il>
2020-04-09 23:28:38 +0300

    Added class Task to BusinessLayer

---
+++ Backend/BusinessLayer/Objects/Task.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.BusinessLayer
+{
+    class Task
+    {
+        private int taskID;
+        private String title;
+        private String description;
+        private DateTime creationDate;
+        private DateTime dueDate;
+        public Task(int taskID,String title, String description, DateTime dueDate)
+        {
+            this.taskID = taskID;
+            this.title = title;
+            this.description = description;
+            this.creationDate = DateTime.Now;
+            this.dueDate = dueDate;
+        }
+        public void EditTitle(String title) { this.title = title; }
+        public void EditDescription(String description) { this.description = description; }
+        public void EditDueDate(DateTime dueDate) { this.dueDate = dueDate; }
+        public int GetTaskID() { return this.taskID; }
+        public DateTime GetCreationDate() { return this.creationDate; }
+        public String GetTitle() { return this.title; }
+        public String GetDescription() { return this.description; }
+        public DateTime GetDueDate() { return this.dueDate; }
+
+    }
+}
