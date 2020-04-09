8d823f212266cb5a7f0aaac5f832677066e2c91f
danielpiro <danielpiro2@gmail.com>
2020-04-09 19:46:31 +0300

    added 2 new classes

---
+++ Backend/BusinessLayer/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class User
+{
+    private string email;
+    private string nickName;
+    private string password;
+
+    public string Email { get => email; set => email = value; }
+    public string NickName { get => nickName; set => nickName = value; }
+    public string Password { get => password; set => password = value; }
+
+    public void Login(string email, string password)
+    {
+        if (this.Password.Equals(password) && this.Email.Equals(email))//verify if the email and password match
+        {
+            Console.WriteLine("login was successfull");
+            this.Login = true;
+        }
+        else
+            throw new Exception("incorrect login information , please enter again");
+    }
+    public void Logout() //updated the user status
+    {
+        this.loggenIn = false;
+    }
+}
---
+++ Backend/BusinessLayer/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+public class UserController
+{
+    private ICollection <string , User>;
+
+    public partial class Email : Form
+    {
+        public Email()
+        {
+            InitializeComponent;
+        }
+        public void Register(string email, string password, string nickName)
+        {
+            this.nickName = nickName;
+
+        }
+        private void EmailCheck(string email) //verify the email pattern
+        {
+            string pattern = "^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$";
+            if (Regex.IsMatch(email, pattern))
+                Console.WriteLine("success");
+            else
+                throw new Exception("please enter valid email");
+
+        }
+    }
+
+}
