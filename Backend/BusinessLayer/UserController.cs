b8d0686c90e491b3868d93ced92319c59265f9f5
danielpiro <danielpiro2@gmail.com>
2020-04-13 11:16:48 +0300

    update namespace

--- Backend/BusinessLayer/UserController.cs
+++ Backend/BusinessLayer/UserController.cs
@@ -7,35 +7,37 @@ using System.Text.RegularExpressions;
 using System.Collections.Generic;
 
 
-
-
-public class UserController
+namespace IntroSE.Kanban.Backend.BusinessLayer
 {
+
+    public class UserController
+    {
         //need to add constractor for creating new users with json
-    private string email;
-    Dictionary<string, User> Users = new Dictionary<string, User>();
-    
+        private string email;
+        Dictionary<string, User> Users = new Dictionary<string, User>();
+
         public User GetUser(string email)
-    {
-        return Users[email];
+        {
+            return Users[email];
+        }
+
+        public void Register(string email, string password, string nickname)
+        {
+            var user = new Users(email, password, nickname);
+            Users.Add(email, user);
+
+        }
+        public bool IsLogged(string email)
+
+        {
+
+            if (Users.ContainsKey(email))
+                return Users[email].IsLogged();
+            return false;
+        }
+
+
+
     }
-
-    public void Register(string email, string password, string nickname)
-    {
-        var user = new Users(email, password, nickname);
-        Users.Add(email,user);
-
-    }
-    public bool IsLogged(string email)
-
-    {
-
-        if (Users.ContainsKey(email))
-             return Users[email].IsLogged();
-        return false;
-    }
-
-
 
 }
-
