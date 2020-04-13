4ee1042043f21fb7c340b1429bac56a77629839b
Bar Rozenman <barroz@post.bgu.ac.il>
2020-04-13 12:16:24 +0300

    New Column

---
+++ Backend/BusinessLayer/Column.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.BusinessLayer.BoardPackage
+{
+    class Column
+    {
+        private int columnOrdinal;
+        private string columnName;
+        private int limit;
+        private List<Task> tasks;
+        public Column(int columnOrdinal, string columnName)
+        {
+            this.columnOrdinal = columnOrdinal;
+            this.columnName = columnName;
+            this.limit = Int32.MaxValue;
+            this.tasks = new List<Task>();
+
+        }
+
+        public int GetColumnOrdinal() { return this.columnOrdinal; }
+        public string GetColumnName() { return this.columnName; }
+        public void SetColumnName(string columnName) { this.columnName = columnName; }
+        public int GetLimit() { return this.limit; }
+        public void LimitColumnTasks(int limit)
+        {
+            if (limit < 1)
+                throw new Exception("Limit not possible");  // it cant be under 1
+            if (limit < tasks.Count)
+                throw new Exception("To much tasks");      // if in the column right now more tasks then the new limit
+            this.limit = limit;
+        }
+        public List<Task> GetTasks() { return this.tasks; }
+
+        public void AddTask(Task newTask)
+        {
+            if (tasks.Count < limit)  // we check if there to much tasks
+                tasks.Add(newTask);
+            else
+                throw new Exception("There is no room for new task");
+        }
+
+        public Task RemoveTask(int taskId)
+        {
+            try
+            {
+                Task getTask = GetTask(taskId);
+                tasks.Remove(getTask); // return true if the occurance delete, exeception if not found
+                return getTask;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public Task GetTask(taskId)
+        {
+            foreach (Task task in tasks)
+            {
+                if (task.GetTaskID() == taskId)
+                    return task;     // we return if the task removed from the column
+            }
+            throw new Exception("The task not found"); // if the task not in the column
+
+        }
+    }
+}
