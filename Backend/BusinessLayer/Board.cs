663bf279246934ede2a313b4b43d3df14be8c3c2
Bar Rozenman <barroz@post.bgu.ac.il>
2020-04-13 11:12:51 +0300

    Update Board

--- Backend/BusinessLayer/Board.cs
+++ Backend/BusinessLayer/Board.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WpfApp2
+namespace IntroSE.Kanban.Backend.BusinessLayer.BoardPackage
 {
     class Board
     {
@@ -13,23 +13,16 @@ namespace WpfApp2
         public Board(string email)
         {
             this.email = email;
-            this.totalTasks = 0;
-            CreatColumns();
-
-        }
-        private void CreatColumns()// Private function that we(the programmers) build the columns.
-        {
             Column backlog = new Column(0, "BackLog");
             Column in_prograss = new Column(1, "In_Prograss");
             Column done = new Column(2, "Done");
             columns.Add(backlog);
-            columns.Add(in_prograss);
+            columns.Add(in_prograss;
             columns.Add(done);
+
         }
-        public int TotalNumberOfTasks()
-        {
-            return GetColumn(0).GetTasks().Count + GetColumn(1).GetTasks().Count + GetColumn(2).GetTasks().Count;
-        }
+       
+       
         public List<Column> GetColumns() { return columns; }
         public Column GetColumn(int columnOrdinal) // we get the key of the column and we return the column with this key
         {
@@ -51,22 +44,7 @@ namespace WpfApp2
             throw new Exception ("There mistake in the columnName");
         }
 
-        public void AdvanceTask(int columnOrdinal,int taskId)
-        {
-            Column addColumn = GetColumn(columnOrdinal+1);
-            Column currenColumn = GetColumn(columnOrdinal);
-            if (addColumn.GetLimit()> addColumn.GetTasks().Count)
-            {
-                Task advTask = currenColumn.GetTask(taskId);
-                addColumn.RemoveTask(advTask);
-                addColumn.AddTask(advTask);
-            }
-            else
-            {
-                Console.WriteLine("There is not ROOM in " + addColumn.GetColumnName());
-            }
-
-        }
+       
     
     }
 }
