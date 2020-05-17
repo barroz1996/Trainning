16242d5f129e72651c8c26859b7aeb93226ec2fd
daniel piro <danielpiro2@gmail.com>
2020-05-17 18:57:33 +0300

    delete class

--- Backend/ServiceLayer/Service.cs
+++ Backend/ServiceLayer/Service.cs
@@ -24,6 +24,7 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
         {
            this.UserController = new BusinessLayer.UserPackage.UserController();
             this.BoardController = new BusinessLayer.BoardPackage.BoardController();
+            LoadData();
         }
                
         /// <summary>        
