60780da30032a9760fa477941d14724c99c8f35b
daniel piro <danielpiro2@gmail.com>
2020-05-20 23:50:49 +0300

    finish assignment 3

--- Backend/ServiceLayer/Service.cs
+++ Backend/ServiceLayer/Service.cs
@@ -17,6 +17,7 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
     	private BusinessLayer.UserPackage.UserController UserController;
         private BusinessLayer.BoardPackage.BoardController BoardController;
         private readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private DataAccessLayer.Controllers.SQLCreator create;
         /// <summary>
         /// Simple public constructor.
         /// </summary>
@@ -24,7 +25,8 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
         {
            this.UserController = new BusinessLayer.UserPackage.UserController();
            this.BoardController = new BusinessLayer.BoardPackage.BoardController();
-           LoadData();
+           create = new DataAccessLayer.Controllers.SQLCreator();
+           
         }
                
         /// <summary>        
